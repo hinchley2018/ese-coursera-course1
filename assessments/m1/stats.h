36202d8520fa35d2e52b72510dbdbfe3ed6f6c10
Code Nurturers LLC <jonathon.hinchley@codenurturers.com>
2026-01-10 07:27:39 -0600

    define functions

--- assessments/m1/stats.h
+++ assessments/m1/stats.h
@@ -9,32 +9,94 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header file for stats.c
  *
- * <Add Extended Description Here>
+ * Defines the interface to the stats functions
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Jonathon Hinchley
+ * @date January 10, 2026
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ *
+ *
+ * @param data array of unsigned char data
+ * @param length length of the data array
+ *
+ * @return no return value
+ */
+void print_statistics(unsigned char *data, unsigned int length);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Given an array of data and a length, prints the array to the screen
  *
- * <Add Extended Description Here>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param data array of unsigned char data
+ * @param length length of the data array
  *
- * @return <Add Return Informaiton here>
+ * @return no return value
  */
+void print_array(unsigned char *data, unsigned int length);
 
+/**
+ * @brief Given an array of data and a length, returns the median value
+ *
+ *
+ * @param data array of unsigned char data
+ * @param length length of the data array
+ *
+ * @return median value of array
+ */
+unsigned char find_median(unsigned char *data, unsigned int length);
+
+/**
+ * @brief Given an array of data and a length, returns the mean
+ *
+ *
+ * @param data array of unsigned char data
+ * @param length length of the data array
+ *
+ * @return mean value of array
+ */
+unsigned char find_mean(unsigned char *data, unsigned int length);
+
+/**
+ * @brief Given an array of data and a length, returns the maximum
+ *
+ *
+ * @param data array of unsigned char data
+ * @param length length of the data array
+ *
+ * @return maximum value in the array
+ */
+unsigned char find_maximum(unsigned char *data, unsigned int length);
+
+/**
+ * @brief Given an array of data and a length, returns the minimum
+ *
+ *
+ * @param data array of unsigned char data
+ * @param length length of the data array
+ *
+ * @return minimum value in the array
+ */
+unsigned char find_minimum(unsigned char *data, unsigned int length);
+
+/**
+ * @brief Given an array of data and a length, sorts the array from largest to smallest. 
+ * (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+ *
+ *
+ * @param data array of unsigned char data
+ * @param length length of the data array
+ *
+ * @return array sorted from largest to smallest
+ */
+unsigned char* sort_array(unsigned char *data, unsigned int length);
 
 #endif /* __STATS_H__ */
